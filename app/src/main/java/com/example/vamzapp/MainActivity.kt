6f50bf3b65a908ae8fe68c15cd18e1e3590fb308
cajka.matej9 <cajka.matej9@gmail.com>
2020-05-19 18:45:59 +0200

    first

---
+++ app/src/main/java/com/example/vamzapp/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.vamzapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val btnNo = findViewById<Button>(R.id.button)
+        val btnYes = findViewById<Button>(R.id.button2)
+        val btnDeath = findViewById<Button>(R.id.button3)
+    }
+
+}
